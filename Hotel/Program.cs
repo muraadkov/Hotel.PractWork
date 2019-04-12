624e8c9fcf60bf6d4dd57df428b2b39d9731bfcf
Адылканов Муратжан Талгатович <@corp.itstep>
2019-04-12 20:03:39 +0600

    практическая работа

---
+++ Hotel/Program.cs
@@ -0,0 +1,20 @@
+﻿using HotelApp.Models;
+using HotelApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main main = new Main();
+            main.Menu();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HotelApp.Models/Hotels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Models
+{
+        public class Hotels
+        {
+        public int Id { get; set; }
+        public int StarRating { get; set; }
+        public string Name { get; set; }
+
+        }
+}
